4e0873f5da9a3647a86cac5e856c90cca836a209
gitbossj <36144783+gitbossj@users.noreply.github.com>
2018-02-06 17:07:23 +0800

    提交

--- src/HadoopTest.java
+++ src/HadoopTest.java
@@ -6,8 +6,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 import java.io.InputStream;
 import java.net.URI;
-
-
+//test
 
 public class HadoopTest {
     public static void main(String[] args) throws Exception {
